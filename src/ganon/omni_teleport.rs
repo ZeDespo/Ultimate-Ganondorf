606dc5f8af6f7c71c4f98bccc9ffd5ffda3683d4
Alexander Despotakis <acdespotakis@gmail.com>
2024-02-19 13:02:23 -0500

    Added omniteleport

---
+++ src/ganon/omni_teleport.rs
@@ -0,0 +1,79 @@
+use super::utils::*;
+use smash::{
+    app::{lua_bind::*, sv_animcmd::*, *},
+    hash40,
+    lib::lua_const::*,
+    lua2cpp::*,
+    phx::*,
+};
+use smash_script::*;
+
+fn is_special_n(motion_kind: u64) -> bool {
+    motion_kind == hash40("special_n")
+}
+
+impl Position2D {
+    unsafe extern "C" fn next_teleport_position(
+        boma: *mut BattleObjectModuleAccessor,
+    ) -> Position2D {
+        Position2D { x: 40.0, y: 40.0 }
+    }
+
+    unsafe extern "C" fn set_to_work_module(self: &Self, boma: *mut BattleObjectModuleAccessor) {
+        WorkModule::set_float(boma, self.x, GANON_TELEPORT_NEW_X_POS);
+        WorkModule::set_float(boma, self.y, GANON_TELEPORT_NEW_Y_POS);
+    }
+}
+
+impl TeleportStatus {
+    fn suspend_kinetic_energy(self: &Self) -> bool {
+        return match self {
+            TeleportStatus::PreTransit | TeleportStatus::Transit | TeleportStatus::End => true,
+            _ => false,
+        };
+    }
+}
+
+// If moving into the air, following changes need to be made for float logic:
+// 1. Set flag in handler to do special case float.
+// 2. Transition to float status kind.
+// 3. In float logic, check for flag.
+//  - If set
+//      - Make Ganondorf visible
+//      - Change Kinetic Type to fall
+//      - Instantly restore CanFloat status.
+pub unsafe extern "C" fn ganon_teleport_handler(fighter: &mut L2CFighterCommon) {
+    let boma = fighter.module_accessor;
+    let ts = TeleportStatus::from_int(WorkModule::get_int(boma, GANON_TELEPORT_WORK_INT));
+    let curr_situation_kind = StatusModule::situation_kind(boma);
+    if !StatusModule::status_kind(boma) == *FIGHTER_STATUS_KIND_SPECIAL_N
+        && !curr_situation_kind == SITUATION_KIND_GROUND
+    {
+        return;
+    }
+    match ts {
+        TeleportStatus::Start => {
+            Position2D::next_teleport_position(boma).set_to_work_module(boma);
+        }
+        TeleportStatus::PreTransit => {
+            PostureModule::add_pos_2d(
+                boma,
+                &Vector2f {
+                    x: WorkModule::get_float(boma, GANON_TELEPORT_NEW_X_POS),
+                    y: WorkModule::get_float(boma, GANON_TELEPORT_NEW_Y_POS),
+                },
+            );
+            WorkModule::set_int(
+                boma,
+                TeleportStatus::Transit as i32,
+                GANON_TELEPORT_WORK_INT,
+            );
+        }
+        _ => {}
+    }
+    if StatusModule::situation_kind(boma) == *SITUATION_KIND_AIR && ts.suspend_kinetic_energy()
+    // && !WorkModule::is_flag(boma, GANON_TELEPORT_INTO_FLOAT_HANDLE_FLAG)
+    {
+        WorkModule::on_flag(boma, GANON_TELEPORT_INTO_FLOAT_INIT_FLAG);
+    }
+}
