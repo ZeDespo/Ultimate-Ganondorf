e926766d4c9b502584acb1111357ad46d04ff9df
Alexander Despotakis <acdespotakis@gmail.com>
2024-02-25 09:53:50 -0500

    Commented out neutral special appears to be stable

--- src/ganon/frame.rs
+++ src/ganon/frame.rs
@@ -1,4 +1,4 @@
-use super::utils::{InitValues, GANON_TELEPORT_INTO_FLOAT_HANDLE_FLAG};
+use super::utils::{in_teleport, InitValues, GANON_TELEPORT_INTO_FLOAT_HANDLE_FLAG};
 use crate::ganon::float::ganon_float;
 use crate::ganon::omni_teleport::ganon_teleport_handler;
 use crate::ganon::warlock_punch::warlock_punch;
@@ -15,7 +15,7 @@ pub unsafe extern "C" fn ganon_frame(fighter: &mut L2CFighterCommon) {
         entry_id: WorkModule::get_int(boma, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize,
         motion_module_frame: MotionModule::frame(boma),
         motion_kind: MotionModule::motion_kind(boma),
-        teleport_into_float: WorkModule::is_flag(boma, GANON_TELEPORT_INTO_FLOAT_HANDLE_FLAG),
+        teleport_into_float: in_teleport(boma),
     };
     println!("{:#?}", iv);
     ganon_teleport_handler(fighter, &iv); //
