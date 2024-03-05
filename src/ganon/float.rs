18231355de81f50413f474eead0947616fe7e54b
Alexander Despotakis <acdespotakis@gmail.com>
2024-03-05 18:12:59 -0500

    Side b shenanigans

--- src/ganon/float.rs
+++ src/ganon/float.rs
@@ -135,6 +135,7 @@ impl FloatStatus {
                     *FIGHTER_STATUS_KIND_ESCAPE_AIR,
                     *FIGHTER_STATUS_KIND_ESCAPE_AIR_SLIDE,
                     *FIGHTER_GANON_STATUS_KIND_SPECIAL_N_TURN,
+                    *FIGHTER_GANON_STATUS_KIND_SPECIAL_AIR_S_CATCH,
                 ]
                 .contains(&init_values.status_kind)
                 || (init_values.status_kind == *FIGHTER_STATUS_KIND_DAMAGE_AIR
