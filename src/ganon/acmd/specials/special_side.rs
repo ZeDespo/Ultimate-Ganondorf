5d4b054008c1a50ea847f66635bca9af7bb34da5
Alexander Despotakis <acdespotakis@gmail.com>
2024-03-13 20:33:11 -0400

    Hitbox on fall

---
+++ src/ganon/acmd/specials/special_side.rs
@@ -0,0 +1,104 @@
+use crate::ganon::utils::in_dive;
+use smash::app::lua_bind::*;
+use smash::app::sv_animcmd::*;
+use smash::lib::lua_const::*;
+use smash_script::{damage, lua_args, macros, slope};
+use {smash::lua2cpp::*, smashline::*};
+
+pub fn install() {
+    Agent::new("ganon")
+        .game_acmd("game_specialairsfall", ganon_specialairsfall)
+        .install();
+}
+
+unsafe extern "C" fn ganon_specialairsfall(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        macros::ATTACK_ABS(
+            agent,
+            *FIGHTER_ATTACK_ABSOLUTE_KIND_CATCH,
+            0,
+            4.0,
+            0,
+            10,
+            0,
+            100,
+            0.0,
+            1.0,
+            *ATTACK_LR_CHECK_F,
+            0.0,
+            true,
+            Hash40::new("collision_attr_normal"),
+            *ATTACK_SOUND_LEVEL_S,
+            *COLLISION_SOUND_ATTR_KICK,
+            *ATTACK_REGION_NONE,
+        );
+        macros::SET_SPEED_EX(agent, 0, -5, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
+    }
+    frame(agent.lua_state_agent, 2.0);
+    if macros::is_excute(agent) {
+        WorkModule::set_int(
+            agent.module_accessor,
+            *FIGHTER_GANON_EXPLOSION_FALL_SETTING_FALL,
+            *FIGHTER_GANON_STATUS_WORK_ID_INT_EXPLOSION_FALL_SETTING,
+        );
+        if in_dive(agent.module_accessor) {
+            macros::ATTACK(
+                agent,
+                1,
+                0,
+                Hash40::new("handl"),
+                14.0,
+                80,
+                100,
+                0,
+                50,
+                3.5,
+                0.0,
+                0.0,
+                0.0,
+                None,
+                None,
+                None,
+                1.0,
+                1.0,
+                *ATTACK_SETOFF_KIND_ON,
+                *ATTACK_LR_CHECK_POS,
+                false,
+                10,
+                0.0,
+                0,
+                false,
+                false,
+                false,
+                false,
+                true,
+                *COLLISION_SITUATION_MASK_GA,
+                *COLLISION_CATEGORY_MASK_ALL,
+                *COLLISION_PART_MASK_ALL,
+                false,
+                Hash40::new("collision_attr_purple"),
+                *ATTACK_SOUND_LEVEL_L,
+                *COLLISION_SOUND_ATTR_KICK,
+                *ATTACK_REGION_KICK,
+            );
+        }
+    }
+}
+
+unsafe extern "C" fn effect_specialairsfall(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        macros::EFFECT_FOLLOW(
+            agent,
+            Hash40::new("ganon_engokua_catch"),
+            Hash40::new("havel"),
+            -1,
+            0,
+            0.5,
+            0,
+            0,
+            0,
+            1,
+            true,
+        );
+    }
+}
---
+++ src/ganon/new_down_special.rs
@@ -0,0 +1,58 @@
+//! Ideal workflow
+//! FLOAT -> SPECIAL_AIR_S_CATCH -> SPECIAL_AIR_S_FALL -> SPECIAL_AIR_S_END
+//!
+//! Frame data for explosion
+//! Frame 0 - 2 -> Small explosion, greatest knockback
+//! Frame 3 - 6 -> Medium explosion medium knockback
+//! Frame 7 - 13 -> Large explosion, least knockback
+//! Frame 14 -> 18 -> meduium explosion, pathetic knockback
+//! Frame 19 - 40 -> End lag.
+use super::utils::*;
+use skyline_smash::app::BattleObjectModuleAccessor;
+use smash::app::lua_bind::*;
+use smash::lib::lua_const::*;
+use smash_script::macros;
+use std::f32::consts::PI;
+use {
+    smash::{hash40, lua2cpp::*},
+    smashline::*,
+};
+
+//
+unsafe extern "C" fn precondition_check(
+    boma: *mut BattleObjectModuleAccessor,
+    iv: &InitValues,
+) -> bool {
+    if let FloatStatus::Floating(_) = GS[iv.entry_id].float_status {
+        if !in_teleport(boma) && iv.status_kind == FIGHTER_STATUS_KIND_SPECIAL_LW {
+            return true;
+        }
+    }
+    false
+}
+
+pub unsafe extern "C" fn new_down_special(fighter: &mut L2CFighterCommon, iv: &InitValues) {
+    let boma = fighter.module_accessor;
+    let dive_check = in_dive(boma);
+    if !dive_check {
+        let pc_check = precondition_check(boma, iv);
+        if pc_check {
+            WorkModule::on_flag(boma, GANON_FLOAT_INTO_DIVE);
+        } else {
+            return;
+        }
+        return;
+    }
+    if iv.status_kind == FIGHTER_STATUS_KIND_SPECIAL_LW {
+        GS[iv.entry_id].float_status = FloatStatus::CannotFloat;
+        StatusModule::change_status_request_from_script(
+            boma,
+            *FIGHTER_GANON_STATUS_KIND_SPECIAL_AIR_S_FALL,
+            false.into(),
+        );
+    } else if iv.status_kind == FIGHTER_GANON_STATUS_KIND_SPECIAL_AIR_S_FALL {
+    } else if iv.status_kind == FIGHTER_GANON_STATUS_KIND_SPECIAL_AIR_S_END {
+    } else {
+        WorkModule::off_flag(boma, GANON_FLOAT_INTO_DIVE);
+    }
+}
