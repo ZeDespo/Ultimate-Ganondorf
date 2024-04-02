d0c8d50d1728ffdf1bf76207e6ba32bfc5bb4192
Alexander Despotakis <acdespotakis@gmail.com>
2024-04-01 22:25:44 -0400

    All palutena files from ghidra

---
+++ ghidra/palutena/special_hi2_end.c
@@ -0,0 +1,9 @@
+void __thiscall
+L2CFighterPalutenaghidra::status::SpecialHi2_end
+          (L2CFighterPalutenaghidra *this,L2CValue *return_value)
+
+{
+  lib::L2CValue::L2CValue((L2CValue *)return_value,0);
+  return;
+}
+
