926897b2d69004c435455b91fd7d5e414b9a2783
FatherOfEgg <fatherofegg@gmail.com>
2024-08-24 12:01:25 -0400

    Moved backhand acmd into its own file

--- src/ganon/acmd/specials/mod.rs
+++ src/ganon/acmd/specials/mod.rs
@@ -1,11 +1,11 @@
 mod special_high;
 mod special_low;
 mod special_neutral;
-mod special_side;
+mod special_side_backhand;
 
 pub fn install() {
     special_neutral::install();
     special_low::install();
-    special_side::install();
+    special_side_backhand::install();
     special_high::install();
 }
