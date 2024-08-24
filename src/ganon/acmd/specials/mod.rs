d22d6edf39ff4dd4219cc065a2f6227753cff674
FatherOfEgg <fatherofegg@gmail.com>
2024-08-24 12:05:08 -0400

    Piledrive acmd when falling. Using frames 1 and 2 from dark_rupture

--- src/ganon/acmd/specials/mod.rs
+++ src/ganon/acmd/specials/mod.rs
@@ -1,11 +1,13 @@
 mod special_high;
 mod special_low;
 mod special_neutral;
+mod special_air_side_fall;
 mod special_side_backhand;
 
 pub fn install() {
     special_neutral::install();
     special_low::install();
+    special_air_side_fall::install();
     special_side_backhand::install();
     special_high::install();
 }
