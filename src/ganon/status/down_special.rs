02e217b43e12e4b6e3c6fa2696b42bb6393b071c
FatherOfEgg <fatherofegg@gmail.com>
2024-08-24 11:55:18 -0400

    New down b status. Piledrive if in the air

---
+++ src/ganon/status/down_special.rs
@@ -0,0 +1,16 @@
+use crate::imports::*;
+
+unsafe extern "C" fn ganon_speciallw_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
+    if fighter.global_table[0x16].get_i32() == *SITUATION_KIND_AIR {
+        StatusModule::set_status_kind_interrupt(fighter.module_accessor, *FIGHTER_GANON_STATUS_KIND_SPECIAL_AIR_S_CATCH);
+        return 1.into();
+    }
+
+    original_status(Pre, fighter, *FIGHTER_STATUS_KIND_SPECIAL_LW)(fighter)
+}
+
+pub fn install() {
+    Agent::new("ganon")
+        .status(Pre, *FIGHTER_STATUS_KIND_SPECIAL_LW, ganon_speciallw_pre)
+        .install();
+}
