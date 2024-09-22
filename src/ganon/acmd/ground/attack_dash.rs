d16f8b2cc1736b2412b18116ea3752cefb6ca861
Alexander Despotakis <acdespotakis@gmail.com>
2024-09-21 22:03:47 -0400

    Dash attack multi-hit achieved. Bug with fsmash hitbox not hitting resolved

--- src/ganon/acmd/ground/attack_dash.rs
+++ src/ganon/acmd/ground/attack_dash.rs
@@ -11,22 +11,29 @@ pub fn install() {
         .install();
 }
 
-unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
-    frame(agent.lua_state_agent, 1.0);
-    macros::FT_MOTION_RATE(agent, 0.8);
-    frame(agent.lua_state_agent, 11.0);
-    macros::FT_MOTION_RATE(agent, 1.0);
-    frame(agent.lua_state_agent, 16.0);
+unsafe extern "C" fn dash_attack_hitbox(agent: &mut L2CAgentBase, angle: u64, damage: f32) {
+    let fkb1: i32;
+    let fkb2: i32;
+    let angle2: u64;
+    if angle >= 360 {
+        fkb1 = 5;
+        fkb2 = 2;
+        angle2 = 368;
+    } else {
+        fkb1 = 0;
+        fkb2 = 0;
+        angle2 = angle;
+    }
     if macros::is_excute(agent) {
         macros::ATTACK(
             agent,
             0,
             0,
             Hash40::new("top"),
-            13.0,
-            53,
+            damage,
+            angle,
             82,
-            0,
+            fkb1,
             67,
             3.0,
             0.0,
@@ -35,7 +42,7 @@ unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
             Some(0.0),
             Some(8.0),
             Some(8.5),
-            1.3,
+            0.5,
             1.0,
             *ATTACK_SETOFF_KIND_ON,
             *ATTACK_LR_CHECK_F,
@@ -54,7 +61,7 @@ unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
             false,
             Hash40::new("collision_attr_purple"),
             *ATTACK_SOUND_LEVEL_L,
-            *COLLISION_SOUND_ATTR_FIRE,
+            *COLLISION_SOUND_ATTR_ELEC,
             *ATTACK_REGION_MAGIC,
         );
         macros::ATTACK(
@@ -62,10 +69,10 @@ unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
             1,
             0,
             Hash40::new("top"),
-            13.0,
-            53,
+            damage,
+            angle2,
             82,
-            0,
+            fkb2,
             67,
             6.0,
             0.0,
@@ -74,7 +81,7 @@ unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
             None,
             None,
             None,
-            1.3,
+            0.5,
             1.0,
             *ATTACK_SETOFF_KIND_ON,
             *ATTACK_LR_CHECK_F,
@@ -93,92 +100,40 @@ unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
             false,
             Hash40::new("collision_attr_purple"),
             *ATTACK_SOUND_LEVEL_L,
-            *COLLISION_SOUND_ATTR_FIRE,
+            *COLLISION_SOUND_ATTR_ELEC,
             *ATTACK_REGION_MAGIC,
         );
+        if angle2 == 368 {
+            AttackModule::set_vec_target_pos(
+                agent.module_accessor,
+                1,
+                Hash40::new("top"),
+                &Vector2f { x: 1.0, y: 0.0 },
+                4,
+                false,
+            );
+        }
     }
-    frame(agent.lua_state_agent, 18.0);
-    if macros::is_excute(agent) {
-        macros::ATTACK(
-            agent,
-            0,
-            0,
-            Hash40::new("top"),
-            7.0,
-            361,
-            82,
-            0,
-            66,
-            2.5,
-            0.0,
-            8.0,
-            4.0,
-            Some(0.0),
-            Some(8.0),
-            Some(8.5),
-            1.0,
-            1.0,
-            *ATTACK_SETOFF_KIND_ON,
-            *ATTACK_LR_CHECK_F,
-            false,
-            0,
-            0.0,
-            0,
-            false,
-            false,
-            false,
-            false,
-            true,
-            *COLLISION_SITUATION_MASK_GA,
-            *COLLISION_CATEGORY_MASK_ALL,
-            *COLLISION_PART_MASK_ALL,
-            false,
-            Hash40::new("collision_attr_purple"),
-            *ATTACK_SOUND_LEVEL_S,
-            *COLLISION_SOUND_ATTR_FIRE,
-            *ATTACK_REGION_MAGIC,
-        );
-        macros::ATTACK(
-            agent,
-            1,
-            0,
-            Hash40::new("top"),
-            7.0,
-            361,
-            82,
-            0,
-            66,
-            4.0,
-            0.0,
-            8.0,
-            13.0,
-            None,
-            None,
-            None,
-            1.0,
-            1.0,
-            *ATTACK_SETOFF_KIND_ON,
-            *ATTACK_LR_CHECK_F,
-            false,
-            0,
-            0.0,
-            0,
-            false,
-            false,
-            false,
-            false,
-            true,
-            *COLLISION_SITUATION_MASK_GA,
-            *COLLISION_CATEGORY_MASK_ALL,
-            *COLLISION_PART_MASK_ALL,
-            false,
-            Hash40::new("collision_attr_purple"),
-            *ATTACK_SOUND_LEVEL_S,
-            *COLLISION_SOUND_ATTR_FIRE,
-            *ATTACK_REGION_MAGIC,
-        );
+}
+
+// 16 - 22
+// Hitbox on 16, 18, 20, with 22 hitting far way.
+unsafe extern "C" fn ganon_attackdash(agent: &mut L2CAgentBase) {
+    frame(agent.lua_state_agent, 1.0);
+    macros::FT_MOTION_RATE(agent, 0.8);
+    frame(agent.lua_state_agent, 11.0);
+    macros::FT_MOTION_RATE(agent, 1.0);
+    frame(agent.lua_state_agent, 16.0);
+    for _ in 0..3 {
+        dash_attack_hitbox(agent, 363, 1.2);
+        wait(agent.lua_state_agent, 2.0);
+        if macros::is_excute(agent) {
+            AttackModule::clear_all(agent.module_accessor);
+        }
     }
     frame(agent.lua_state_agent, 22.0);
+    dash_attack_hitbox(agent, 53, 7.7);
+    frame(agent.lua_state_agent, 23.0);
     if macros::is_excute(agent) {
         AttackModule::clear_all(agent.module_accessor);
     }
