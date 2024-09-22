4ac009604950668d19d284bf92368600faf59cd2
Alex Despotakis <acdespotakis@gmail.com>
2024-09-21 23:38:51 -0400

    Created explosion helper functions

--- src/ganon/acmd/specials/special_air_side.rs
+++ src/ganon/acmd/specials/special_air_side.rs
@@ -1,135 +1,68 @@
 //! This function is for the ground/landing hitbox for piledrive
 use crate::{ganon::utils::GANON_DOWN_SPECIAL_AIR_DURATION_FLAG, imports::*};
 
-unsafe extern "C" fn game_specialairs(agent: &mut L2CAgentBase) {
+unsafe extern "C" fn explosion_hitbox(agent: &mut L2CAgentBase, id: u64, damage: f32, angle: u64, size: f32, kbg: i32, bkb: i32) {
     let increase_power = WorkModule::is_flag(agent.module_accessor, GANON_DOWN_SPECIAL_AIR_DURATION_FLAG);
+    let shield_damage = if id == 0 {4} else {1};
+    let (mut d, mut s) = (damage, size);
+    let (mut k, mut b) = ( kbg, bkb);
+    if increase_power {
+        d *= 1.5;
+        s *= 1.5;
+        k += 20;
+        b += 20;
+    }
+    if macros::is_excute(agent) {
+        macros::ATTACK(
+            agent,
+            id,
+            0,
+            Hash40::new("handl"),
+            d,
+            angle,
+            k,
+            0,
+            b,
+            s,
+            0.0,
+            0.0,
+            0.0,
+            None,
+            None,
+            None,
+            1.0,
+            1.0,
+            *ATTACK_SETOFF_KIND_ON,
+            *ATTACK_LR_CHECK_POS,
+            false,
+            shield_damage,
+            0.0,
+            0,
+            false,
+            false,
+            false,
+            false,
+            true,
+            *COLLISION_SITUATION_MASK_GA,
+            *COLLISION_CATEGORY_MASK_ALL,
+            *COLLISION_PART_MASK_ALL,
+            false,
+            Hash40::new("collision_attr_cutup"),
+            *ATTACK_SOUND_LEVEL_L,
+            *COLLISION_SOUND_ATTR_FIRE,
+            *ATTACK_REGION_MAGIC,
+        );
+    }
+
+}
+
+unsafe extern "C" fn game_specialairs(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 1.0);
-    if macros::is_excute(agent) {
-        macros::ATTACK(
-            agent,
-            0,
-            0,
-            Hash40::new("handl"),
-            20.0,
-            361,
-            100,
-            0,
-            40,
-            5.5,
-            0.0,
-            0.0,
-            0.0,
-            None,
-            None,
-            None,
-            1.0,
-            1.0,
-            *ATTACK_SETOFF_KIND_ON,
-            *ATTACK_LR_CHECK_POS,
-            false,
-            0,
-            0.0,
-            0,
-            false,
-            false,
-            false,
-            false,
-            true,
-            *COLLISION_SITUATION_MASK_GA,
-            *COLLISION_CATEGORY_MASK_ALL,
-            *COLLISION_PART_MASK_ALL,
-            false,
-            Hash40::new("collision_attr_cutup"),
-            *ATTACK_SOUND_LEVEL_L,
-            *COLLISION_SOUND_ATTR_FIRE,
-            *ATTACK_REGION_MAGIC,
-        );
-    }
+    explosion_hitbox(agent, 0, 20.0, 361, 5.5, 100, 40);
     frame(agent.lua_state_agent, 3.0);
-    if macros::is_excute(agent) {
-        macros::ATTACK(
-            agent,
-            1,
-            0,
-            Hash40::new("handl"),
-            12.0,
-            73,
-            80,
-            0,
-            20,
-            10.0,
-            0.0,
-            0.0,
-            0.0,
-            None,
-            None,
-            None,
-            1.0,
-            1.0,
-            *ATTACK_SETOFF_KIND_ON,
-            *ATTACK_LR_CHECK_POS,
-            false,
-            0,
-            0.0,
-            0,
-            false,
-            false,
-            false,
-            false,
-            true,
-            *COLLISION_SITUATION_MASK_GA,
-            *COLLISION_CATEGORY_MASK_ALL,
-            *COLLISION_PART_MASK_ALL,
-            false,
-            Hash40::new("collision_attr_cutup"),
-            *ATTACK_SOUND_LEVEL_L,
-            *COLLISION_SOUND_ATTR_FIRE,
-            *ATTACK_REGION_MAGIC,
-        );
-    }
+    explosion_hitbox(agent, 1, 12.0, 73, 10.0, 80, 30);
     frame(agent.lua_state_agent, 7.0);
-    let size = if increase_power {48.0} else {18.0};
-    if macros::is_excute(agent) {
-        macros::ATTACK(
-            agent,
-            2,
-            0,
-            Hash40::new("handl"),
-            8.0,
-            45,
-            60,
-            0,
-            20,
-            size,
-            0.0,
-            0.0,
-            0.0,
-            None,
-            None,
-            None,
-            1.0,
-            1.0,
-            *ATTACK_SETOFF_KIND_ON,
-            *ATTACK_LR_CHECK_POS,
-            false,
-            0,
-            0.0,
-            0,
-            false,
-            false,
-            false,
-            false,
-            true,
-            *COLLISION_SITUATION_MASK_GA,
-            *COLLISION_CATEGORY_MASK_ALL,
-            *COLLISION_PART_MASK_ALL,
-            false,
-            Hash40::new("collision_attr_cutup"),
-            *ATTACK_SOUND_LEVEL_L,
-            *COLLISION_SOUND_ATTR_FIRE,
-            *ATTACK_REGION_MAGIC,
-        );
-    }
+    explosion_hitbox(agent, 2, 8.0, 45, 18.0, 70, 20);
     frame(agent.lua_state_agent, 14.0);
     if macros::is_excute(agent) {
         AttackModule::clear_all(agent.module_accessor);
