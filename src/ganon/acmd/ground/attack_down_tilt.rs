e9d39a285715ba187c1ece8c4b915afdfaeab515
Alexander Despotakis <acdespotakis@gmail.com>
2024-10-03 21:40:41 -0400

    Articles aren't working. Don't know why.

--- src/ganon/acmd/ground/attack_down_tilt.rs
+++ src/ganon/acmd/ground/attack_down_tilt.rs
@@ -1,10 +1,18 @@
-use crate::ganon::utils::{BomaExt, GANON_DOWN_TILT_2_FLAG};
+use crate::ganon::utils::{
+    BomaExt, FIGHTER_GANON_GENERATE_ARTICLE_BACK_AIR, GANON_DOWN_TILT_2_FLAG,
+};
 use crate::imports::*;
 
 /// Down tilt part 1 attack hitboxes.
 unsafe extern "C" fn ganon_down_tilt_1(agent: &mut L2CAgentBase) {
     frame(agent.lua_state_agent, 8.0);
     if macros::is_excute(agent) {
+        ArticleModule::generate_article(
+            agent.module_accessor,
+            FIGHTER_GANON_GENERATE_ARTICLE_BACK_AIR,
+            false,
+            -1,
+        );
         macros::ATTACK(
             agent,
             0,
@@ -130,6 +138,135 @@ unsafe extern "C" fn ganon_down_tilt_expression(agent: &mut L2CAgentBase) {
     }
 }
 
+unsafe extern "C" fn ganon_projectile(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        macros::ATTACK(
+            agent,
+            0,
+            0,
+            Hash40::new("top"),
+            5.0,
+            361,
+            20,
+            0,
+            35,
+            2.4,
+            0.0,
+            0.0,
+            0.0,
+            None,
+            None,
+            None,
+            0.6,
+            1.0,
+            *ATTACK_SETOFF_KIND_ON,
+            *ATTACK_LR_CHECK_SPEED,
+            false,
+            -2.5,
+            0.0,
+            0,
+            true,
+            true,
+            false,
+            false,
+            false,
+            *COLLISION_SITUATION_MASK_GA,
+            *COLLISION_CATEGORY_MASK_ALL,
+            *COLLISION_PART_MASK_ALL,
+            false,
+            Hash40::new("collision_attr_magic"),
+            *ATTACK_SOUND_LEVEL_S,
+            *COLLISION_SOUND_ATTR_MAGIC,
+            *ATTACK_REGION_NONE,
+        );
+        AttackModule::enable_safe_pos(agent.module_accessor);
+    }
+    frame(agent.lua_state_agent, 5.0);
+    if macros::is_excute(agent) {
+        macros::ATTACK(
+            agent,
+            0,
+            0,
+            Hash40::new("top"),
+            5.0,
+            361,
+            15,
+            0,
+            28,
+            2.2,
+            0.0,
+            0.0,
+            0.0,
+            None,
+            None,
+            None,
+            0.6,
+            1.0,
+            *ATTACK_SETOFF_KIND_ON,
+            *ATTACK_LR_CHECK_SPEED,
+            false,
+            -2.5,
+            0.0,
+            0,
+            true,
+            true,
+            false,
+            false,
+            false,
+            *COLLISION_SITUATION_MASK_GA,
+            *COLLISION_CATEGORY_MASK_ALL,
+            *COLLISION_PART_MASK_ALL,
+            false,
+            Hash40::new("collision_attr_magic"),
+            *ATTACK_SOUND_LEVEL_S,
+            *COLLISION_SOUND_ATTR_MAGIC,
+            *ATTACK_REGION_NONE,
+        );
+    }
+    frame(agent.lua_state_agent, 30.0);
+    if macros::is_excute(agent) {
+        macros::ATTACK(
+            agent,
+            0,
+            0,
+            Hash40::new("top"),
+            4.0,
+            361,
+            10,
+            0,
+            22,
+            2.0,
+            0.0,
+            0.0,
+            0.0,
+            None,
+            None,
+            None,
+            0.6,
+            1.0,
+            *ATTACK_SETOFF_KIND_ON,
+            *ATTACK_LR_CHECK_SPEED,
+            false,
+            -2,
+            0.0,
+            0,
+            true,
+            true,
+            false,
+            false,
+            false,
+            *COLLISION_SITUATION_MASK_GA,
+            *COLLISION_CATEGORY_MASK_ALL,
+            *COLLISION_PART_MASK_ALL,
+            false,
+            Hash40::new("collision_attr_magic"),
+            *ATTACK_SOUND_LEVEL_S,
+            *COLLISION_SOUND_ATTR_MAGIC,
+            *ATTACK_REGION_NONE,
+        );
+    }
+}
+
 pub fn install() {
     Agent::new("ganon")
         .game_acmd("game_attacklw3", ganon_down_tilt, Priority::Default)
@@ -138,5 +275,8 @@ pub fn install() {
             ganon_down_tilt_expression,
             Priority::Default,
         )
+        .install();
+    Agent::new("ganon_backairprojectile")
+        .game_acmd("game_regular", ganon_projectile, Priority::Default)
         .install();
 }
